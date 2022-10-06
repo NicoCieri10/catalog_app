329bd3129ba2f97dbba02b425701d6e556e04d23
NicoCieri10 <nicocieri16@gmail.com>
2022-10-05 23:16:33 -0300

    Creating new project

---
+++ form_validation/lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:form_validation/app/app.dart';
+import 'package:form_validation/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
