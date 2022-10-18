aef344063bf76e5a3dbe831493a378fe1bae8245
NicoCieri10 <nicocieri16@gmail.com>
2022-10-18 20:09:31 -0300

    Update product_page.dart

--- lib/product/product_page.dart
+++ lib/product/product_page.dart
@@ -1,5 +1,8 @@
+import 'package:appsize/appsize.dart';
 import 'package:flutter/material.dart';
+import 'package:form_validation/ui/ui.dart';
 import 'package:form_validation/widgets/widgets.dart';
+import 'package:go_router/go_router.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
@@ -13,13 +16,119 @@ class ProductPage extends StatelessWidget {
         child: Column(
           children: [
             Stack(
-              children: const [
-                ProductImage(),
+              children: [
+                const ProductImage(),
+                Positioned(
+                  top: 60.sp,
+                  left: 20.sp,
+                  child: IconButton(
+                    onPressed: context.pop,
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 60.sp,
+                  right: 20.sp,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            const _ProductForm(),
+            SizedBox(
+              height: 100.sp,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.save_outlined,
+        ),
+        onPressed: () {},
+      ),
     );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.sp),
+      child: Container(
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 15.sp,
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10.sp,
+                ),
+                TextFormField(
+                  decoration: InputDecorations.authInputDecoration(
+                    hintText: 'name',
+                    labelText: 'Nombre: ',
+                  ),
+                ),
+                SizedBox(
+                  height: 30.sp,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecorations.authInputDecoration(
+                    hintText: r'$111.11',
+                    labelText: 'Precio: ',
+                  ),
+                ),
+                SizedBox(
+                  height: 30.sp,
+                ),
+                SwitchListTile.adaptive(
+                  value: true,
+                  title: const Text('Disponible'),
+                  activeColor: Colors.blueGrey,
+                  onChanged: (value) {},
+                ),
+                SizedBox(
+                  height: 30.sp,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(25.sp),
+          bottomLeft: Radius.circular(25.sp),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            offset: const Offset(0, 5),
+            blurRadius: 5,
+          ),
+        ],
+      );
 }
