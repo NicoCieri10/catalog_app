d3a2b61f1ccadb63716bedcf0b0734201185996c
NicoCieri10 <nicocieri16@gmail.com>
2022-10-21 00:28:14 -0300

    ProductClient

--- package/product_client/lib/src/product_client.dart
+++ package/product_client/lib/src/product_client.dart
@@ -1,11 +1,28 @@
 import 'package:product_client/src/models/models.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 /// {@template movies_client}
 /// The Client used to make requests to the Product Database.
 /// {@endtemplate}
 class ProductClient {
+  ProductClient() {
+    this.loadProducts();
+  }
+
   static const _baseUrl =
       'flutter-catalog-app-9a394-default-rtdb.firebaseio.com';
 
   final List<Product> products = [];
+
+  //ToDo implementar cubit/TryCatch
+  bool isLoading = true;
+
+  Future<List<Product>> loadProducts() async {
+    final url = Uri.https(_baseUrl, 'products.json');
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
+    print(productsMap);
+  }
 }
